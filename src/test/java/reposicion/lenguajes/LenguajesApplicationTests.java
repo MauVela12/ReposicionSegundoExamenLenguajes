ca6f02f350e0593a09ca0c18912a478f23e5847f
Mauricio Velasquez <vemauricio20@gmail.com>
2024-04-29 16:25:21 -0600

    primer commit

---
+++ src/test/java/reposicion/lenguajes/LenguajesApplicationTests.java
@@ -0,0 +1,13 @@
+package reposicion.lenguajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenguajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
